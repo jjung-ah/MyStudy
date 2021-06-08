e917932872ed070701967d39b681b08e2e00cf83
jjung-ah <rurounida88@gmail.com>
2021-06-08 10:32:02 +0900

    Create algorithm folder

--- 1_ListSum.py
+++
@@ -1,6 +0,0 @@
-# 리스트 원소의 합
-
-def solution(x):
-    answer = 0
-    answer = x[0] + x[len(x)-1]
-    return answer
---
+++ algorithm/1_ListSum.py
@@ -0,0 +1,6 @@
+# 리스트 원소의 합
+
+def solution(x):
+    answer = 0
+    answer = x[0] + x[len(x)-1]
+    return answer
