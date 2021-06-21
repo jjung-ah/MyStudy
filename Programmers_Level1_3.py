888ed45312b928ec5516caca93a36159521b52c1
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-06-21 10:48:04 +0900

    Delete Programmers_Level1_3.py

--- Programmers_Level1_3.py
+++
@@ -1,9 +0,0 @@
-# Programmers_Level1_3.두 정수 사이의 합
-def solution(a,b):
-    
-    list = [a,b]
-    list.sort()    
-    c = [i for i in range(list[0], list[1]+1)]    
-    res = sum(c)
-    
-    return res
