a092edf9b5e507e6a304f4f81d4baa45533d6c0b
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-08-07 23:30:00 +0900

    Add files via upload

---
+++ Append_Extend.py
@@ -0,0 +1,29 @@
+x = ['Tick', 'Tock', 'Song']
+y = ['Ping', 'Pong']
+x.append(y)
+print('x:', x)
+
+x = ['Tick', 'Tock', 'Song']
+y = ['Ping', 'Pong']
+x.extend(y)
+print('x:', x)
+
+x = ['Tick', 'Tock', 'Song']
+y = [['Ping', 'Pong']]
+x.append(y)
+print('x:', x)
+
+x = ['Tick', 'Tock', 'Song']
+y = [['Ping', 'Pong']]
+x.extend(y)
+print('x:', x)
+
+x = ['Tick', 'Tock', 'Song']
+y = 'Ping'
+x.append(y)
+print('x:', x)
+
+x = ['Tick', 'Tock', 'Song']
+y = 'Ping'
+x.extend(y)
+print('x:', x)
