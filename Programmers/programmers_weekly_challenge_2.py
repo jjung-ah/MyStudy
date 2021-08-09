43593521d005bb15544fc81744455dd50177eef2
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-08-10 00:21:31 +0900

    Update programmers_weekly_challenge_2.py

--- Programmers/programmers_weekly_challenge_2.py
+++ Programmers/programmers_weekly_challenge_2.py
@@ -1,10 +1,33 @@
 def solution(scores):
     answer = ''
+    score = 0
     for i in range(len(scores)):
         my_score = []
-        for scorelist in scores:
-            #print(scorelist[i])
-            my_score.append(scorelist[i])
-        print(my_score)
+        for j in range(len(scores)):
+            #print(scores[j][i])
+            my_score.append(scores[j][i])
+        #print(scores[i][i])    
+        #print(my_score)
+        if scores[i][i] == max(my_score) or scores[i][i] == min(my_score):
+            last_score = sum(my_score) - scores[i][i]
+            score = last_score / (len(scores)-1)
+            #print(score)
+        else:
+            last_score = sum(my_score)
+            score = last_score / len(scores)
+            #print(score)
+
+        if score >= 90:
+            answer += 'A'
+        elif score >= 80:
+            answer += 'B'
+        elif score >= 70:
+            answer += 'C'
+        elif score >= 60:
+            answer += 'D'
+        elif score >= 50:
+            answer += 'E'
+        else:
+            answer += 'F'
 
     return answer
