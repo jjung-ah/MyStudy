77ba73beb90e4b951189ac69852e500f6c330903
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-08-20 23:57:09 +0900

    Update 4_image_pixel.py

--- image_example/4_image_pixel.py
+++ image_example/4_image_pixel.py
@@ -6,3 +6,13 @@ cv2.imshow('original', img)
 
 subimg = img[300:400, 350:750]
 cv2.imshow('cutting', subimg)
+
+img[300:400, 0:400]
+
+print(img.shape)
+print(subimg.shape)
+
+cv2.imshow('modified', img)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
