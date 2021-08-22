00798d6fd8b1eba9fff39388c6e425cef3f009ab
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-08-22 23:33:51 +0900

    Update Programmers_Level1_22.py

--- Programmers/Level1/Programmers_Level1_22.py
+++ Programmers/Level1/Programmers_Level1_22.py
@@ -9,3 +9,18 @@ def solution(arr1, arr2):
             line.append(num)
         answer.append(line)
     return answer
+
+'''
+def solution(arr1, arr2):
+    #answer = []
+    answer = [[0 for j in range(len(arr2))] for i in range(len(arr1))]
+    for i in range(len(arr1)):
+        #line = []
+        #print(arr1[i])
+        for j in range(len(arr2)):
+            #print(arr2[j])
+            num = arr1[i][j] + arr2[i][j]
+            answer[i][j] = num
+
+    return answer
+'''
