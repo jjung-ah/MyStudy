ee2677518dc144c3f4346f36af2e3fc0a792e858
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-08-25 23:26:03 +0900

    Update Programmers_Level1_22.py

--- Programmers/Level1/Programmers_Level1_22.py
+++ Programmers/Level1/Programmers_Level1_22.py
@@ -24,3 +24,14 @@ def solution(arr1, arr2):
 
     return answer
 '''
+'''
+a = []    # 빈 리스트 생성
+ 
+for i in range(2):
+    line = []              # 안쪽 리스트로 사용할 빈 리스트 생성
+    for j in range(1):
+        line.append(0)     # 안쪽 리스트에 0 추가
+    a.append(line)         # 전체 리스트에 안쪽 리스트를 추가
+ 
+print(a)
+'''
