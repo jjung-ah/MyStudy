d7e71ba0e695fb4373f07f6bfde1e0eff57c7b63
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-09-18 23:18:04 +0900

    Update Programmers_Level1_33.py

--- Programmers/Level1/Programmers_Level1_33.py
+++ Programmers/Level1/Programmers_Level1_33.py
@@ -1,11 +1,11 @@
-# 문자열 내 p와 y의 개수   # 다시 풀어야 함
+# 문자열 내 p와 y의 개수
 
 def solution(s):
     answer = True
     if 'p' not in s and 'y' not in s:
         return True
     else:
-        if lower(s).count('p') == lower(s).count('y'):
-            return true
+        if s.lower().count('p') == s.lower().count('y'):
+            return True
         else:
-            return false
+            return False
