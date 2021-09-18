a34e3fe80cac3a56dceb9d2cd9e92f4b04d0a18e
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-09-19 00:06:51 +0900

    Add files via upload

---
+++ Programmers/Level1/Programmers_Level1_34.py
@@ -0,0 +1,12 @@
+# 없는 숫자 더하기
+
+def solution(numbers):
+    answer = 45
+    answer = answer - sum(numbers)
+    return answer
+
+
+
+# 실행 코드 
+solution([1,2,3,4,6,7,8,0])   # 14
+solution([5,8,4,0,6,7,9]   # 6
