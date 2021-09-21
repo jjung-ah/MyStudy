792154e79dfb3f1e93ca15470426cf5f2cff9417
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-09-22 00:01:07 +0900

    Update Programmers_Level1_35.py

--- Programmers/Level1/Programmers_Level1_35.py
+++ Programmers/Level1/Programmers_Level1_35.py
@@ -1,5 +1,6 @@
-# 소수 찾기   # 다시하기   # 답은 나오지만 런타임에러
+# 소수 찾기   # 다시하기   
 
+# Version_1   # 답은 나오지만 런타임에러
 def prime_number(x):   # 소수인지 판별하는 함수
     for i in range(2, x):   # 2부터 x-1 까지 모든 수를 확인하면서
         if x % i == 0:   # x가 나누어 떨어지는 수가 있다면 
@@ -18,3 +19,18 @@ def solution(n):
 # 실행 코드
 solution(10)   # 4
 solution(5)   # 3
+
+
+
+# Version_2   # 마찬가지로 답은 나오지만 런타임에러
+def solution(n):   # 주어진 숫자보다 작은 수 중에 소수가 있는지 판별하는 함수
+    answer = 0
+    for x in range(2, n+1):   # 주어진 숫자보다 작은 수 중에 소수가 있는지 확인하는 루프
+        b = 0
+        for i in range(2, x):   # 2부터 x-1 까지 모든 수를 확인하면서
+            if x % i == 0:   # x가 나누어 떨어지는 수가 있다면 
+                b = 1   # 소수가 아니다
+        if b == 0:   # 소수이면
+            answer += 1   # 카운트 +1 
+
+    return answer
