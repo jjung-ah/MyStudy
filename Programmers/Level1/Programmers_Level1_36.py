dcbd155af4216cbb2519bd760ffcb44cc3c34a36
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-09-24 00:34:26 +0900

    Update Programmers_Level1_36.py

--- Programmers/Level1/Programmers_Level1_36.py
+++ Programmers/Level1/Programmers_Level1_36.py
@@ -1,8 +1,11 @@
-# 문자열 다루기 기본   # 다시 하기
+# 문자열 다루기 기본   # 문자열의 길이가 4 또는 6인지, 그리고 숫자인지 문자인지 판별하는 함수
 
 def solution(s):
     #answer = True
-    return s.isdigit()
+    if len(s) == 4 or len(s) == 6:
+        return s.isdigit()
+    return False
+
     #return s.lstrip("-").isdigit()
     #return s.isnumeric()
 
