6bfc895913201d375249dbe3e50943eeba86ccd5
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-09-28 15:12:43 +0900

    Update Programmers_Level1_38.py

--- Programmers/Level1/Programmers_Level1_38.py
+++ Programmers/Level1/Programmers_Level1_38.py
@@ -5,10 +5,24 @@ def solution(s):
     answer = ''
     for idx, num in enumerate(string):
         if num in s:
+            print(num, idx)
             s = s.replace(num, str(idx))
+            print(s)
         answer = s
     return int(answer)
 
 
 s1 = "one4seven56eightnine"
 solution(s1)   # 1475689
+
+'''
+('one', 1)
+14seven56eightnine
+('seven', 7)
+14756eightnine
+('eight', 8)
+147568nine
+('nine', 9)
+1475689
+1475689
+'''
