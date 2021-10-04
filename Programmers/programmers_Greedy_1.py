240936b4fd21b1296fcb162597890f7567b286cf
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-10-04 11:43:47 +0900

    Update programmers_Greedy_1.py

--- Programmers/programmers_Greedy_1.py
+++ Programmers/programmers_Greedy_1.py
@@ -1,14 +1,33 @@
-# 체육복_다시 해보기
+# 체육복_다시 해보기  # 맞는데 런타임에러
 def solution(n, lost, reserve):
     #answer = 0
-    survive = []
-    for i in reserve:
-        pre = i - 1
-        back = i + 1
-        if pre in lost:
-            survive.append(pre)
-        elif back in lost:
-            survive.append(back)
+    PE = [[i, 1] for i in range(1, n+1)]
+    #print(PE)
+    #print(PE[2][0])
 
-    answer = n - len(lost) + len(set(survive))
+    for i in range(n):
+        if PE[i][0] in lost:
+            PE[i][1] = 0
+        elif PE[i][0] in reserve:
+            PE[i][1] = 2
+    #print(PE)
+
+
+    for i in range(n):
+        pre = i-1
+        next = i+1
+        if PE[i][1]==0 and PE[pre][1]==2:
+            PE[i][1] = 1
+            PE[pre][1] = 1
+        elif PE[i][1]==0 and PE[next][1]==2:
+            PE[i][1] = 1
+            PE[next][1] = 1
+
+
+    answer = 0
+    for i in range(n):
+        if PE[i][1] == 1 or PE[i][1] == 2:
+            answer += 1
+
+    #print(answer)
     return answer
