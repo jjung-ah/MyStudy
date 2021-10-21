6426713dd69022f7f6e9f76b4be202a2cb6f49a4
jjung-ah <71995801+jjung-ah@users.noreply.github.com>
2021-10-21 23:19:26 +0900

    Update programmers_DFS_BFS_1.py

--- Programmers/programmers_DFS_BFS_1.py
+++ Programmers/programmers_DFS_BFS_1.py
@@ -19,3 +19,30 @@ def solution(n, computers):
                     visited[i] = 1
         answer += 1
     return answer
+
+
+
+'''
+import numpy as np 
+answers = [1,2,3,4,5]
+#answers = [1,3,2,4,2]
+
+p1 = [1, 2, 3, 4, 5]*2000
+p2 = [2, 1, 2, 3, 2, 4, 2, 5]*1250
+p3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]*1000
+p1 = p1[:len(answers)]
+p2 = p2[:len(answers)]
+p3 = p3[:len(answers)]
+eq1 = list(np.equal(p1, answers)).count(True)
+eq2 = list(np.equal(p2, answers)).count(True)
+eq3 = list(np.equal(p3, answers)).count(True)
+answer = [eq1, eq2, eq3]
+
+print(answer)
+best = []
+for idx,p in enumerate(answer, start=1):
+    if p == max(answer):
+        best.append(idx)
+
+print(sorted(best))
+'''
